d4b70f43648ce0962ce3514530d8dc9a8a6597d3
Kelly Jefferies <k.jefferies@gmail.com>
2022-03-08 22:40:58 -0600

    Base code on HTML, CSS, and JS files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # code-quiz
+
+
+
+https://www.youtube.com/watch?v=LQGTb112N_c
+
+https://www.youtube.com/watch?v=riDzcEQbX6k
--- script.js
+++ script.js
@@ -1,48 +1,118 @@
-
 var timeleft = 60;
 
+let questionContainer = document.getElementById('question-container')
+let questionElement = document.getElementById('question')
+let nextButton = document.getElementById('next')
+let startButton = document.getElementById('start')
+let answerTrue = document.getElementById('answer-true')
+let answerFalse = document.getElementById('answer-false')
+let questionIndex = 0
+let answers = []
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", checkCriteria);
+startButton.addEventListener('click', startQuiz)
+nextButton.addEventListener('click', nextQuestion)
 
+// first time initalization
+function startQuiz() {
+  startButton.classList.add('hidden')
+  nextButton.classList.remove('hidden')
+  // answerBox.classList.remove('hidden')
+  showQuestion(questionBank[questionIndex])
+  questionIndex += 1
+}
 
-//Countdown Timer
+function checkValue () {
+  var selectedAnswer;
 
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
+  if(answerTrue.checked) {
+    selectedAnswer = 'true';
+  }
+  if(answerFalse.checked) {
+    selectedAnswer = 'false';
+  }  
+  return selectedAnswer;
+}
+
+// helper function to advance question state
+function nextQuestion() {
+  if (questionIndex >= questionBank.length) {
+    nextButton.innerText = "Finish"
+  }
+  else {
+    answers.push(checkValue())
+    resetQuestionState()
+    showQuestion(questionBank[questionIndex])
+    questionIndex += 1
+  }
+}
+
+// cleanup HTML back to baseline
+function resetQuestionState() {
+  answerTrue.checked = false;
+  answerFalse.checked = false;
+}
+//Turn question bank answers into an array of strings
+//questionBank.map(obj => obj.answer)
+
+// modifies DOM elements to display question object supplied
+function showQuestion(question) {
+  questionElement.innerText = question.prompt
+}
+
+var downloadTimer = setInterval(function () {
+  if (timeleft <= 0) {
     clearInterval(downloadTimer);
     document.getElementById("countdown").innerHTML = "Times Up";
-  } else {
+  }
+  else {
     document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
     document.getElementById("progressBar").value = timeleft;
   }
   timeleft -= 1;
 }, 1000);
 
+function calcScore(grade) {
+  grade = ((grade / questionBank.length) * 100);
+  alert("You got a " + grade + "% on this quiz");
+}
 
+var count = localStorage.getItem("count");
 
-// Questions for flash cards
-/*
-the number five plus the string five equals 10
-false *
-true
----
-JavaScript is a server side programming language only.
-true
-false *
-----
-JavaScript was created in 1995
-true *
-false
------
-Div is a semantic tag.
- true
- false *
+let questionBank = [
+  {
+    prompt: "The number 5 plus the string five equals 10.",
+    answer: "b",
+  },
+  {
+    prompt: "JavaScript is a server side programming language only. ",
+    answer: "b",
+  },
+  {
+    prompt: "JavaScript was created in 1995. ",
+    answer: "a",
+  },
+  {
+    prompt: "Div is a semantic tag. ",
+    answer: "b",
+  },
+  {
+    prompt: "You link the sytle sheet within the HTML. ",
+    answer: "a",
+  },
+]
 
- ---
-You create flexbox containers within the CSS
- true *
- false
+// document.getElementById("startButton").addEventListener("click");
 
+// function askQuestions() {
+//   var score = 0;
+//   for (var i = 0; i < questionBank.length; i++) {
+//     var response = window.prompt(questionBank[i].prompt);
+//     if (response == questionBank[i].answer) {
+//       score++;
+//     }
+//   }
+//   calcScore(score);
+// }
 
-*/
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", checkCriteria);
