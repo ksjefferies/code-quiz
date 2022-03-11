bdc1de65dbf03471ff27e92410193a9eb0007df2
Kelly Jefferies <k.jefferies@gmail.com>
2022-03-10 19:15:24 -0600

    Refactor code, added local storage to JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,220 @@
-// HTML DOM modifiers
+// HTML DOM Get modifiers
 let startButton = document.getElementById('start');
+let scoreboardButton = document.getElementById('viewHigh')
+let nextButton = document.getElementById('next');
+let resetButton = document.getElementById('reset')
 let answerTrue = document.getElementById('answer-true');
 let answerFalse = document.getElementById('answer-false');
-let nextButton = document.getElementById('next');
 let questionElement = document.getElementById('question');
-var progressBar = document.getElementById('progressBar');
-var input = document.getElementsByTagName('input')
-var labels = document.getElementsByTagName('label');
-var resetButton = document.getElementById('reset')
-var resetScore = document.getElementById('resetScore')
-var scoreboardButton = document.getElementById('viewHigh')
-var scoreList = document.getElementById('scoreList')
-var createLi = document.createElement('li')
-var scoreboard = []
+let progressBar = document.getElementById('progressBar');
+let countdown = document.getElementById('countdown');
+let resetScore = document.getElementById('resetScore')
+let scoreList = document.getElementById('scoreList')
 
+let input = document.getElementsByTagName('input');
+let labels = document.getElementsByTagName('label');
+
+let createLi = document.createElement('li')
+
+// HTML DOM Set attributes
 progressBar.setAttribute('value', '0', 'max', '60');
 input[0].setAttribute('type', 'radio', 'name', 'True', 'value', 'true');
 input[1].setAttribute('type', 'radio', 'name', 'False', 'value', 'false');
 
+// Event Listeners
 startButton.addEventListener('click', startQuiz)
 nextButton.addEventListener('click', nextQuestion)
 scoreboardButton.addEventListener('click', displayScoreboard)
 resetButton.addEventListener('click', resetPage)
-resetScore.addEventListener('click', resetHighScore)
 
 answerTrue.addEventListener('click', buttonTrueCheck)
 answerFalse.addEventListener('click', buttonFalseCheck)
 
 // Global Variables
-let secondsLeft = 60;
-let questionIndex = 0;
-let answers = []
+let secondsLeft = 45; // Timer initial setting
+let questionIndex = 0; // Question array index
+let answers = [] // Input answers empty array
+let scoreboard = []
+let vis = 0;  // //Increment visibility value
+let downloadTimer // timer variable
+
+// First time initalization including timer start
+function startQuiz() {
+  visibility(vis)
+  startTimer();
+  showQuestion(questionBank[questionIndex])
+}
+
+// Helper function to advance question state
+function nextQuestion() {
+  if (questionIndex >= questionBank.length - 1) {
+    vis++;
+    visibility(vis)
+    answers.push(checkValue())
+    clearInterval(downloadTimer);
+    calcScore(answers)
+  }
+  else {
+    answers.push(checkValue())
+    penalty()
+    resetQuestionState()
+    questionIndex += 1
+    showQuestion(questionBank[questionIndex])
+  }
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.prompt
+}
+
+// check state of radio box and sets true and false value
+function checkValue() {
+  let selectedAnswer;
+  if (answerTrue.checked) {
+    selectedAnswer = 'true';
+  }
+  if (answerFalse.checked) {
+    selectedAnswer = 'false';
+  }
+  return selectedAnswer;
+}
+
+// decrement time value on false answers
+function penalty() {
+  let i = questionIndex
+
+  if (answers.slice(-1) != questionBank[i].answer) {
+    secondsLeft -= 10;
+  }
+  return;
+}
+
+// Calc total score by comparing given answers with answers in question bank
+function calcScore(score) {
+  let grade = 0;
+  let trueAnswers = 0
+
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] == questionBank[i].answer) {
+      trueAnswers++
+    }
+    grade = ((trueAnswers / score.length) * 10000)
+  }
+  highScore(grade);
+}
+
+// Retrieve high scores from local storage
+function getHighScore() {
+  var highList = localStorage.getItem('highScoreList')
+  if (highList === null) {
+    highList = []
+  } else {
+    highList = JSON.parse(highList)
+  }
+  return highList
+}
+
+//Prompt user for name and pass final score to place into local storage
+function highScore(score) {
+  visibility(3);
+  var initials = prompt("Enter your initials")
+  var highList = getHighScore()
+
+  highList.push({
+    user: initials,
+    highScore: score,
+  });
+
+  highList = highList.sort((a, b) => (a.highScore < b.highScore ? 1 : -1))
+  if (highList.length > 3) {
+    highList.pop();
+  }
+
+  var stringInfo = JSON.stringify(highList);
+
+  localStorage.setItem('highScoreList', stringInfo)
+}
+
+// Appends the high scores list item into the HTML from local storage
+function displayScoreboard() {
+  var pulledItem = getHighScore()
+  for (let i = 0; i < pulledItem.length; i++) {
+    var createLi = document.createElement('li');
+    createLi.innerHTML = pulledItem[i].user + " - " + pulledItem[i].highScore
+    scoreList.appendChild(createLi)
+  }
+}
+
+// cleanup HTML back to baseline
+function resetQuestionState() {
+  answerTrue.checked = false;
+  answerFalse.checked = false;
+}
+
+// prevent both buttons from being selected at same time
+function buttonTrueCheck() {
+  if (answerTrue.checked) {
+    answerFalse.checked = false;
+  }
+}
+
+function buttonFalseCheck() {
+  if (answerFalse.checked) {
+    answerTrue.checked = false;
+  }
+}
+
+// Reset page when try again is selected
+function resetPage() {
+  location.reload();
+}
+
+// Global visibility settings
+// Start of quiz
+function visibility(value) {
+  if (value === 0) {
+    startButton.classList.add('hidden');
+    nextButton.classList.remove('hidden');
+    labels[0].classList.remove('hidden');
+    labels[1].classList.remove('hidden');
+    input[0].classList.remove('hidden');
+    input[1].classList.remove('hidden');
+    progressBar.classList.remove('hidden');
+    countdown.classList.remove('hidden');
+  }
+
+  // All Questions complete
+  if (value === 1) {
+    nextButton.classList.add('hidden')
+    labels[0].classList.add('hidden')
+    labels[1].classList.add('hidden')
+    input[0].classList.add('hidden')
+    input[1].classList.add('hidden')
+    progressBar.classList.add('hidden')
+    countdown.classList.add('hidden')
+    scoreboardButton.classList.remove("hidden")
+  }
+
+  // Quiz Complete, screen cleared all except Try again
+  if (value === 2) {
+    resetButton.classList.remove('hidden')
+    questionElement.classList.add('hidden')
+  }
+
+  // Quiz not complete, but time has run out settings
+  if (value === 3) {
+    nextButton.classList.add('hidden')
+    labels[0].classList.add('hidden')
+    labels[1].classList.add('hidden')
+    input[0].classList.add('hidden')
+    input[1].classList.add('hidden')
+    progressBar.classList.add('hidden')
+    countdown.classList.add('hidden')
+    scoreboardButton.classList.remove("hidden")
+    resetButton.classList.remove('hidden')
+    questionElement.classList.add('hidden')
+  }
+}
 
 //Questions / Answers Array of Objects
 let questionBank = [
@@ -58,140 +242,19 @@ let questionBank = [
 
 // This function will start the timer countdown
 function startTimer() {
-  var downloadTimer = setInterval(function () {
+  downloadTimer = setInterval(function () {
     if (secondsLeft <= 0) {
       clearInterval(downloadTimer);
       document.getElementById('countdown').innerHTML = 'Times Up';
+      if (answers.length < questionBank) {
+        answers.push(checkValue());
+      }
+      calcScore(answers);
     }
     else {
       document.getElementById('countdown').innerHTML = secondsLeft + ' seconds remaining';
       progressBar.value = secondsLeft;
     }
-    
     secondsLeft -= 1;
-    if(secondsLeft <= 0) {
-      resetPage();
-    }
   }, 1000);
-}
-
-// First time initalization including timer start
-function startQuiz() {
-  startButton.classList.add('hidden')
-  nextButton.classList.remove('hidden')
-  labels[0].classList.remove('hidden')
-  labels[1].classList.remove('hidden')
-  input[0].classList.remove('hidden')
-  input[1].classList.remove('hidden')
-  startTimer();
-  console.log(questionBank[questionIndex])
-  showQuestion(questionBank[questionIndex])
- 
-}
-
-// helper function to advance question state
-function nextQuestion() {
-  if (questionIndex >= questionBank.length - 1) {
-    answers.push(checkValue())
-    calcScore(answers)
-    // nextButton.innerText = 'Try Again';
-    // highScore();
-    nextButton.classList.add('hidden')
-    scoreboardButton.classList.remove("hidden")
-    resetButton.classList.add('hidden');
-  }
-  else {
-    answers.push(checkValue())
-    penalty()
-    resetQuestionState()
-    questionIndex += 1
-    showQuestion(questionBank[questionIndex])
-    
-  }
-}
-
-function checkValue() {
-  var selectedAnswer;
-  if (answerTrue.checked) {
-    selectedAnswer = 'true';
-  }
-  if (answerFalse.checked) {
-    selectedAnswer = 'false';
-  }
-  return selectedAnswer;
-}
-
-function penalty() {
-  let i = questionIndex
-
-  if (answers.slice(-1) != questionBank[i].answer) {
-    secondsLeft -= 10;
-  }
-  return;
-}
-
-function calcScore(score) {
-  let grade = 0;
-  let trueAnswers = 0
-
-  for (var i = 0; i < score.length; i++) {
-    if (score[i] == questionBank[i].answer) {
-      trueAnswers++
-    }
-    grade = ((trueAnswers / score.length) * 10000)
-  }
-  highScore(grade);
-}
-
-function buttonTrueCheck() {
-  if (answerTrue.checked) {
-    answerFalse.checked = false;
-  }
-}
-
-function buttonFalseCheck() {
-  if (answerFalse.checked) {
-    answerTrue.checked = false;
-  }
-}
-
-function displayScoreboard() {
-  questionElement.classList.add('hidden')
-  labels[0].classList.add('hidden')
-  labels[1].classList.add('hidden')
-  input[0].classList.add('hidden')
-  input[1].classList.add('hidden')
-  scoreboardButton.classList.add('hidden')
-  resetButton.classList.remove('hidden')
-  // console.log("THIS WORKS");
-  for (let i = 0; i < scoreboard.length; i++) {
-    var createLi = document.createElement('li');
-    createLi.innerHTML = scoreboard[i].initials + " - " + scoreboard[i].score
-    scoreList.appendChild(createLi)
-  }
-}
-
-// cleanup HTML back to baseline
-function resetQuestionState() {
-  answerTrue.checked = false;
-  answerFalse.checked = false;
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.prompt
-}
-
-function highScore(score) {
-  var initials = prompt("Enter your initials")
-  scoreboard.push({ initials, score });
-  scoreboard.sort((a, b) => (a.score > b.score ? 1 : -1))
-  scoreboardButton
-}
-
-function resetHighScore() {
-  scoreboard.splice(0, scoreboardArr.length)
-}
-
-function resetPage() {
-  location.reload();
 }
--- README.md
+++ README.md
@@ -1,7 +1,41 @@
-# code-quiz
+# Code Quiz
 
+## Objectives
 
+In this project, i did the following:
 
-https://www.youtube.com/watch?v=LQGTb112N_c
+- User will click 'Start Quiz' and be given 5 True / False Questions
+- Click the 'Start' button will start a 60 second countdown timer
+- Correct buttons are hidden when not needed and appear when needed.
+- When incorrect answers are given, time is deducted
+- When all questions are answered or time has expired, the quiz ends
+- When game is over, user is prompted to enter their initials and name and score are saved on High Score list
+- Player High Score list is stored locally
+- Player can click button to see the high score list or click try again to restart quiz
 
-https://www.youtube.com/watch?v=riDzcEQbX6k
+-----
+
+## Final screenshots of initial and results page
+
+![Screenshot of initial Password Generator webpage](./assets/images/password-generator-initial.png)
+
+![Screenshot of final Password Generator webpage](./assets/images/password-generator-results.png)
+
+-----
+## Links for Github Repository and final public page
+
+- [Github Repository for Code Quiz](https://github.com/ksjefferies/code-quiz)
+
+- [Code Quiz final page](https://ksjefferies.github.io/code-quiz/)
+
+## Contributions and sources:
+
+- [Creating online Quizzes](https://www.youtube.com/watch?v=LQGTb112N_c)
+- [Creating online Quizzed](https://www.youtube.com/watch?v=riDzcEQbX6k)
+
+I worked with the following people:
+- Daniel Norred
+- Eugene
+
+## License
+This code is licensed under the MIT License
